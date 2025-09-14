3962b949d1656ad6255405af745a129d9244ef33
Ben Brochet <ben5627@cloud.com>
2025-09-13 20:31:29 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let button = document.fgetElementById(githubLink);
+
+githubItem.addEventListener("click", function() {
+    window.open("https://github.com/BenBrochet", "_blank");
+});
+
+let linkdinItem = document.getElementById("linkdinLink");
+
+/*linkdinItem.addEventListener("click", function() {
+    window.open("https://linkedin.com/in/YOUR_ID","_blank");
+});
+*/
